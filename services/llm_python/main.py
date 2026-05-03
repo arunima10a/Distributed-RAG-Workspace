c351a522af00e2f4b27be19f44024e822cefdad6
arunima10a <arunima10a@gmail.com>
2026-05-03 14:15:32 +0530

    env update

--- services/llm_python/main.py
+++ services/llm_python/main.py
@@ -7,6 +7,8 @@ import sys
 from concurrent import futures
 from sentence_transformers import SentenceTransformer
 import requests
+from dotenv import load_dotenv
+load_dotenv()
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 project_root = os.path.abspath(os.path.join(current_dir, "../../"))
